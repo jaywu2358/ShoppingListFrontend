f2a446624d37f7b6ecd258d73a818e926a14d1e3
unknown <jaywu2358@gmail.com>
2023-02-09 12:24:14 +0800

    Tried axios instead of baseurl

--- vue/src/services/ShoppingListService.js
+++ vue/src/services/ShoppingListService.js
@@ -1,51 +1,49 @@
 import axios from 'axios';
 import store from '../store/index';
 
-const http = axios.create({
-    baseURL: "https://shoppinglistapp-backend.onrender.com"
-});
+
 
 export default {
 
     createShoppingList(shoppingList) {
         this.state = store.state;
-        return http.post(`lists/`, shoppingList);
+        return axios.post(`/lists/`, shoppingList);
     },
 
     viewAllItemsByListId(listId) {
-        
-        return http.get(`lists/${listId}`);
+        this.state = store.state;
+        return axios.get(`/lists/${listId}`);
     },
 
     viewShoppingListByListId(listId) {
         this.state = store.state;
-        return http.get(`lists/list/${listId}`);
+        return axios.get(`/lists/list/${listId}`);
     },
 
     viewShoppingListsByAccountId() {
         this.state = store.state;
-        return http.get(`lists/account/mylists`);
+        return axios.get(`/lists/account/mylists`);
     },
 
     viewShoppingListsByGroupId(groupId) {
         this.state = store.state;
-        return http.get(`lists/group?groupId=${groupId}`);
+        return axios.get(`/lists/group?groupId=${groupId}`);
     },
 
     removeShoppingList(listId) {
         this.state = store.state;
         const id = this.state.user.id;
-        return http.delete(`lists/delete/${listId}?accountId=${id}`);
+        return axios.delete(`/lists/delete/${listId}?accountId=${id}`);
     },
 
     clearList(listId) {
         this.state = store.state;
-        return http.delete(`lists/delete?listId=${listId}`);
+        return axios.delete(`/lists/delete?listId=${listId}`);
     },
 
     claimShoppingList(listId,shoppingList) {
         this.state = store.state;
-        return http.put(`lists/${listId}/claim`,shoppingList);
+        return axios.put(`/lists/${listId}/claim`,shoppingList);
     }
 
 }
