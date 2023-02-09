86916c6a95b6ed5372a53700ffc3f054aa2b91cd
unknown <jaywu2358@gmail.com>
2023-02-09 12:12:34 +0800

    Updated / to all endpoints

--- vue/src/services/ItemService.js
+++ vue/src/services/ItemService.js
@@ -2,21 +2,21 @@ import axios from 'axios';
 import store from '../store/index';
 
 const http = axios.create({
-    baseURL: "https://shoppinglistapp-backend.onrender.com/"
+    baseURL: "https://shoppinglistapp-backend.onrender.com"
 });
 
 export default {
     addItem(item) {
         this.state = store.state;
-        return http.post(`lists/createItem`, item)
+        return http.post(`/lists/createItem`, item)
     },
 
     deleteItem(id) {
         this.state = store.state;
-        return http.delete(`lists/deleteItem?itemId=${id}`)
+        return http.delete(`/lists/deleteItem?itemId=${id}`)
     },
     modifyItem(id, item) {
         this.state = store.state;
-        return http.put(`lists/items/${id}`, item)
+        return http.put(`/lists/items/${id}`, item)
     }
 }
--- vue/src/services/ShoppingListService.js
+++ vue/src/services/ShoppingListService.js
@@ -2,50 +2,50 @@ import axios from 'axios';
 import store from '../store/index';
 
 const http = axios.create({
-    baseURL: "https://shoppinglistapp-backend.onrender.com/"
+    baseURL: "https://shoppinglistapp-backend.onrender.com"
 });
 
 export default {
 
     createShoppingList(shoppingList) {
         this.state = store.state;
-        return http.post(`lists/`, shoppingList);
+        return http.post(`/lists/`, shoppingList);
     },
 
     viewAllItemsByListId(listId) {
         
-        return http.get(`lists/${listId}`);
+        return http.get(`/lists/${listId}`);
     },
 
     viewShoppingListByListId(listId) {
         this.state = store.state;
-        return http.get(`lists/list/${listId}`);
+        return http.get(`/lists/list/${listId}`);
     },
 
     viewShoppingListsByAccountId() {
         this.state = store.state;
-        return http.get(`lists/account/mylists`);
+        return http.get(`/lists/account/mylists`);
     },
 
     viewShoppingListsByGroupId(groupId) {
         this.state = store.state;
-        return http.get(`lists/group?groupId=${groupId}`);
+        return http.get(`/lists/group?groupId=${groupId}`);
     },
 
     removeShoppingList(listId) {
         this.state = store.state;
         const id = this.state.user.id;
-        return http.delete(`lists/delete/${listId}?accountId=${id}`);
+        return http.delete(`/lists/delete/${listId}?accountId=${id}`);
     },
 
     clearList(listId) {
         this.state = store.state;
-        return http.delete(`lists/delete?listId=${listId}`);
+        return http.delete(`/lists/delete?listId=${listId}`);
     },
 
     claimShoppingList(listId,shoppingList) {
         this.state = store.state;
-        return http.put(`lists/${listId}/claim`,shoppingList);
+        return http.put(`/lists/${listId}/claim`,shoppingList);
     }
 
 }
