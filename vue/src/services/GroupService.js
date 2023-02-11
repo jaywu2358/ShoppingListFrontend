9c92c1874ef8eb22a3f3e6f6dd7973f4ffee14e4
unknown <jaywu2358@gmail.com>
2023-02-11 11:41:29 +0800

    Changed return method from http to axois

--- vue/src/services/GroupService.js
+++ vue/src/services/GroupService.js
@@ -1,81 +1,78 @@
 import axios from 'axios';
 import store from '../store/index';
 
-const http = axios.create({
-    baseURL: "https://shoppinglistapp-backend.onrender.com"
-});
 
 export default {
 
     getGroupByGroupId(id) {
         this.state = store.state;
-        return http.get(`groups/${id}`)
+        return axios.get(`groups/${id}`)
     },
 
     createGroup(group) {
         this.state = store.state;
-        return http.post(`groups/`, group)
+        return axios.post(`groups/`, group)
     },
 
     createShoppingListInGroup(shoppingList, groupId) {
         this.state = store.state;
-        return http.post(`groups/list/${groupId}`, shoppingList)
+        return axios.post(`groups/list/${groupId}`, shoppingList)
     },
 
     viewAllGroupsByUsername(username) {
         this.state = store.state;
-        return http.get(`groups/?username=${username}`)
+        return axios.get(`groups/?username=${username}`)
     },
 
 
     viewMyInvitations(accountId) {
         this.state = store.state;
-        return http.get(`groups/invitation/${accountId}`)
+        return axios.get(`groups/invitation/${accountId}`)
     },
 
 
     viewSentInvitations(accountId) {
         this.state = store.state;
-        return http.get(`groups/sentInvitation/${accountId}`)
+        return axios.get(`groups/sentInvitation/${accountId}`)
     },
 
     inviteUserToGroup(invite) {
         this.state = store.state;
-        return http.post(`groups/invitation/`, invite)
+        return axios.post(`groups/invitation/`, invite)
     },
 
     addUserToGroup(groupId, invite) {
         this.state = store.state;
-        return http.post(`groups/${groupId}`, invite)
+        return axios.post(`groups/${groupId}`, invite)
     },
 
     updateInviteStatus(invite) {
         this.state = store.state;
-        return http.put(`groups/invitation/`, invite)
+        return axios.put(`groups/invitation/`, invite)
     },
 
     deleteUserFromGroup(accountId, groupId) {
         this.state = store.state;
-        return http.delete(`groups/leave/?accountId=${accountId}&groupId=${groupId}`)
+        return axios.delete(`groups/leave/?accountId=${accountId}&groupId=${groupId}`)
     },
 
     findAllUsers(username) {
         this.state = store.state;
-        return http.get(`groups/users/${username}`)
+        return axios.get(`groups/users/${username}`)
     },
 
     getUserIdByUsername(username) {
         this.state = store.state;
-        return http.get(`groups/getId/${username}`)
+        return axios.get(`groups/getId/${username}`)
     },
     rejectInvite(inviteId) {
         this.state = store.state;
-        return http.delete(`groups/reject/${inviteId}`)
+        return axios.delete(`groups/reject/${inviteId}`)
     },
 
     getUsernameByUserId(userId) {
         this.state = store.state;
-        return http.get(`groups/getUsername/${userId}`)
+        return axios.get(`groups/getUsername/${userId}`)
     }
     
 }
--- vue/src/services/ShoppingListService.js
+++ vue/src/services/ShoppingListService.js
@@ -7,42 +7,50 @@ export default {
 
     createShoppingList(shoppingList) {
         this.state = store.state;
+        
         return axios.post(`/lists/`, shoppingList);
     },
 
     viewAllItemsByListId(listId) {
         this.state = store.state;
+        
         return axios.get(`/lists/${listId}`);
     },
 
     viewShoppingListByListId(listId) {
         this.state = store.state;
+        
         return axios.get(`/lists/list/${listId}`);
     },
 
     viewShoppingListsByAccountId() {
         this.state = store.state;
+        
         return axios.get(`/lists/account/mylists`);
     },
 
     viewShoppingListsByGroupId(groupId) {
         this.state = store.state;
+        
         return axios.get(`/lists/group?groupId=${groupId}`);
     },
 
     removeShoppingList(listId) {
         this.state = store.state;
         const id = this.state.user.id;
+        
         return axios.delete(`/lists/delete/${listId}?accountId=${id}`);
     },
 
     clearList(listId) {
         this.state = store.state;
+        
         return axios.delete(`/lists/delete?listId=${listId}`);
     },
 
     claimShoppingList(listId,shoppingList) {
         this.state = store.state;
+        
         return axios.put(`/lists/${listId}/claim`,shoppingList);
     }
 
